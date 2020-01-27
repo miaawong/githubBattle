21d62bd58590b1cf4a65c01469cb361669eba4d2
Mia Wong <miaw629@gmail.com>
2020-01-27 17:16:39 -0500

    setState with class component, then learned  passing props to functional component to setState

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,25 +1,51 @@
 import React, { Component } from "react";
 
+const LanguagesNav = ({ selected, onUpdateLanguage }) => {
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    return (
+        <ul className="flex-center">
+            {languages.map(language => (
+                <li key={language}>
+                    <button
+                        className="btn-clear nav-link"
+                        // passing it an arrow function, so that it won't be invoked unless it's clicked
+                        // you don't want to do onClick={this.updateLanguage(language)}
+                        // ^^ that will invoked immediately
+                        style={language === selected ? { color: "red" } : null}
+                        onClick={() => onUpdateLanguage(language)}
+                    >
+                        {language}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
 export default class Popular extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: "All"
+        };
+        // updateLanguage's context is above ^^, not the function LanguageNav context
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        });
+    }
     render() {
-        const languages = [
-            "All",
-            "JavaScript",
-            "Ruby",
-            "Java",
-            "CSS",
-            "Python"
-        ];
+        const { selectedLanguage } = this.state;
         return (
-            <ul className="flex-center">
-                {languages.map(language => (
-                    <li key={language}>
-                        <button className="btn-clear nav-link">
-                            {language}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <React.Fragment>
+                <LanguagesNav
+                    selected={selectedLanguage}
+                    onUpdateLanguage={this.updateLanguage}
+                ></LanguagesNav>
+            </React.Fragment>
         );
     }
 }
