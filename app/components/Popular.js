085ec6f1817bce8116b213d32d2e9de0520b65cc
Mia Wong <miaw629@gmail.com>
2020-02-01 16:32:36 -0500

    turned repos into an obj;doesn't fetch after its been fetched, some UI added

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { fetchPopularRepos } from "../utils/api";
+import {
+    FaUser,
+    FaStar,
+    FaCodeBranch,
+    FaExclamationTriangle
+} from "react-icons/fa";
 
 const LanguagesNav = ({ selected, onUpdateLanguage }) => {
     const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -27,12 +33,73 @@ LanguagesNav.propTypes = {
     selected: PropTypes.string.isRequired,
     onUpdateLanguage: PropTypes.func.isRequired
 };
+function ReposGrid({ repos }) {
+    return (
+        <ul className="grid space-around">
+            {repos.map((repo, index) => {
+                const {
+                    name,
+                    owner,
+                    html_url,
+                    stargazers_count,
+                    forks,
+                    open_issues
+                } = repo;
+                const { login, avatar_url } = owner;
+                return (
+                    <li key={html_url} className="repo bg-light">
+                        <h4 className="header-lg center-text">#{index + 1}</h4>
+                        <img
+                            className="avatar"
+                            src={avatar_url}
+                            alt={`Avatar for ${login}`}
+                        />
+                        <h2 className="center-text">
+                            <a className="link" href={html_url}>
+                                {login}
+                            </a>
+                        </h2>
+                        <ul className="card-list">
+                            <li>
+                                <FaUser color="rgb(255,191, 116)" size={22} />
+                                <a href={`https://github.com/${login}`}>
+                                    Login
+                                </a>
+                            </li>
+                            <li>
+                                <FaStar color="rgb(255,215,0)" size={22} />
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+                            <li>
+                                <FaCodeBranch
+                                    color="rgb(129,195, 245)"
+                                    size={22}
+                                />
+                                {forks.toLocaleString()} forks
+                            </li>
+                            <li>
+                                <FaExclamationTriangle
+                                    color="rgb(241, 138,147)"
+                                    size={22}
+                                />
+                                {open_issues.toLocaleString()} open issues
+                            </li>
+                        </ul>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+ReposGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+};
 export default class Popular extends Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedLanguage: "All",
-            repos: null,
+            repos: {},
             error: null
         };
         // updateLanguage's context is above ^^, not the function LanguageNav context
@@ -46,25 +113,31 @@ export default class Popular extends Component {
     updateLanguage(selectedLanguage) {
         this.setState({
             selectedLanguage,
-            error: null,
-            repos: null
+            error: null
         });
-        fetchPopularRepos(selectedLanguage)
-            .then(repos =>
-                this.setState({
-                    repos,
-                    error: null
+        // fetch if the selectedlanuage doesn't exist in our repos obj
+        if (!this.state.repos[selectedLanguage]) {
+            fetchPopularRepos(selectedLanguage)
+                .then(data => {
+                    // we are updating the new repos based on the current repos
+                    this.setState(({ repos }) => ({
+                        repos: {
+                            ...repos,
+                            [selectedLanguage]: data
+                        }
+                    }));
                 })
-            )
-            .catch(() => {
-                console.warn("Error fetching repos: ", error);
-                this.setState({
-                    error: "There was an error fetching the repos"
+                .catch(() => {
+                    console.warn("Error fetching repos: ", error);
+                    this.setState({
+                        error: "There was an error fetching the repos"
+                    });
                 });
-            });
+        }
     }
     isLoading() {
-        return this.state.repos === null && this.state.error === null;
+        const { selectedLanguage, repos, error } = this.state;
+        return !repos[selectedLanguage] && error === null;
     }
     render() {
         const { selectedLanguage, repos, error } = this.state;
@@ -76,7 +149,9 @@ export default class Popular extends Component {
                 />
                 {this.isLoading() && <p>LOADING</p>}
                 {error && <p>{error}</p>}
-                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+                {repos[selectedLanguage] && (
+                    <ReposGrid repos={repos[selectedLanguage]} />
+                )}
             </React.Fragment>
         );
     }
